3c97c6cad32aba5f60875dd474949484813f813d
JuanCruzIses <juancruzises@gmail.com>
2023-12-05 19:20:05 -0300

    Adding new configuration in dbConnection

--- src/database/index.js
+++ src/database/index.js
@@ -1,17 +1,21 @@
 import mongoose from "mongoose"
-
+const dbPass = process.env.DB_PASS
+const dbName = 'ecommerce'
+const urlDB = `mongodb+srv://juancruzises:${dbPass}@ecommerce.ymc2wbm.mongodb.net/${dbName}`
 const configOptions = {
     useNewUrlParser : true,
     useUnifiedTopology: true
 }
 
 const connectToDB = async()=>{
-    const connectionUrl = 'mongodb+srv://juancruzises:MatiMacheTunkiLeo@ecommerce.ymc2wbm.mongodb.net/'
-    mongoose.connect(connectionUrl, configOptions)
-    .then(()=>
-        console.log('Database connected succesfully'))
-        .catch((err)=>
-            console.log(`Error in connection DB: ${err.message}`))
+    try{
+        const connectionUrl = urlDB
+        await mongoose.connect(connectionUrl, configOptions)
+        console.log('Database connected succesfully')
+
+    }catch(err){
+        console.log(`Error in connection DB: ${err.message}`)
+    }
 }
 
 export default connectToDB
